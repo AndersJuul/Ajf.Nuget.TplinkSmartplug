04919be4dba6c4f9588cb9dc61551a7b37f7f5eb
AndersJuul <andersjuulsfirma@gmail.com>
2018-04-25 18:39:07 +0200

    a

---
+++ Src/Ajf.Nuget.TplinkSmartplug.Tests/DummyFixture.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+
+namespace Ajf.Nuget.TplinkSmartplug.Tests
+{
+    [TestFixture]
+    public class DummyFixture   
+    {
+        [Test]
+        public void TestNothing()
+        {
+
+        }
+    }
+}
